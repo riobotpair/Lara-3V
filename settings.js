9170c97d63e587f7501482deda999f069faa168c
riobotpair <dena74137@gmail.com>
2025-08-12 16:36:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SMoRlKaC#kAzAvcONY7oLY5q0jNK72uA_RM6Nxg1giT4IcKM2BWI", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "zMI01KzR#0UAYmsMmUtistUfOEfkdJqJqduWG5kPnexr3vw9zOfc", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94720630290",
